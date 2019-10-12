8f99012678dba14c8f6b5170c9b51358c193fb9e
Grgur Grisogono <grgur@grgur.com>
2019-10-12 14:39:56 +0200

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 // @ts-nocheck
-import React from 'react';
-import Menu from './components/Menu';
+import React, { Suspense, lazy } from 'react';
 import Hero from './components/Hero';
-import Sidebar from './components/Sidebar';
-import Products from './components/Products';
-import BoringContent from './components/BoringContent';
-import Footer from './components/Footer';
+import Menu from './components/Menu';
 import './App.css';
+
+const Sidebar = lazy(() => import('./components/Sidebar'));
+const Products = lazy(() => import('./components/Products'));
+const BoringContent = lazy(() => import('./components/BoringContent'));
+const Footer = lazy(() => import('./components/Footer'));
 
 const App = () => (
   <>
     <Menu />
-    <Sidebar />
+    <Suspense fallback={<div />}>
+      <Sidebar />
+    </Suspense>
     <div id="main">
       <Hero />
       <main>
-        <Products />
-        <BoringContent />
+        <Suspense fallback={<div />}>
+          <Products />
+        </Suspense>
+        <Suspense fallback={<div />}>
+          <BoringContent />
+        </Suspense>
       </main>
     </div>
-    <Footer />
+    <Suspense fallback={<div />}>
+      <Footer />
+    </Suspense>
   </>
 );
 
